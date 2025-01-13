a61b7e387afb79af7ea1b807252c7f32921819ae
Akshit Patel <akshitpatel.g@gmail.com>
2025-01-13 11:51:07 +0530

    Day 2 submission

--- Day1/level1/avgpercent.java
+++
@@ -1,9 +0,0 @@
-class avgpercent{
-	public static void main(String args[]){
-		int maths=94,physics=95,chemistry=96;
-		
-		int average=(maths+physics+chemistry)/3;  // Average= Sum of values / number of values
-		System.out.println("Sam's average mark in PCM is "+average);
-
-	}
-}
